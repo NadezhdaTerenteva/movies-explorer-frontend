c7efaed0f1a74b32811df18aaca4a118f0d715b0
Nadezhda Terenteva <nadya_09@inbox.ru>
2022-10-29 01:02:55 +0300

    запросы к mainApi, context

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,61 +1,169 @@
-import React, { useState } from "react";
-import { Route, Switch } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Route, Switch } from "react-router-dom";
 
-import Main from '../Main/Main';
-import Movies from '../Movies/Movies';
-import SavedMovies from '../SavedMovies/SavedMovies';
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
-import Profile from '../Profile/Profile';
-import Register from '../Register/Register';
-import Login from '../Login/Login';
-import NotFound from '../NotFound/NotFound';
-import SidebarMenu from '../SidebarMenu/SidebarMenu';
+import { mainApi } from "../../utils/MainApi.js";
 
-import './App.css';
+import Main from "../Main/Main";
+import MoviesCard from "../MoviesCard/MoviesCard.js";
+import Movies from "../Movies/Movies";
+import SavedMovies from "../SavedMovies/SavedMovies";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import Profile from "../Profile/Profile";
+import Register from "../Register/Register";
+import Login from "../Login/Login";
+import NotFound from "../NotFound/NotFound";
+import SidebarMenu from "../SidebarMenu/SidebarMenu";
 
+import "./App.css";
+
+import { CurrentUserContext } from "../../contexts/CurrentUserContext.js";
 
 function App() {
 
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [currentUser, setCurrentUser] = useState({ name: "", email: "" });
+
+  const [movies, setMovies] = useState([]);
+
+  const [statusMessage, setStatusMessage] = useState(true);
+
+  const [tooltipMessage, setTooltipMessage] = useState("");
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const history = useHistory();
+
+  // Получаем данные пользователя
+  useEffect(() => {
+    if (isLoggedIn)
+      mainAapi
+        .getUserInfo()
+        .then((res) => {
+          setCurrentUser(res)
+          setIsLoggedIn(true)
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+  }, [isLoggedIn]);
+
+  // Получаем сохраненные фильмы
+  useEffect(() => {
+    if (isLoggedIn)
+      mainApi
+        .getMovies()
+        .then((moviesList) => {
+          
+          if (moviesList) {
+            setMovies(moviesList)
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+  }, [isLoggedIn]);
+
+  const movieList = movies.map((item) => {
+    return (
+      <MoviesCard
+        key={item._id}
+        card={item}
+        // onCardClick={handleCardClick}
+        // onCardLike={handleCardLike}
+        // onCardDelete={handleCardDelete}
+      />
+    );
+  });
+
+  function handleUpdateUser(userData) {
+    mainApi
+      .updateUser(userData)
+      .then((res) => {
+        setCurrentUser(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  function onLogin(data) {
+    const { email, password } = data;
+    return mainApi
+      .login(email, password)
+      .then((res) => {
+        setIsLoggedIn(true);
+      })
+      .catch((err) => {
+        setStatusMessage(false);
+        setTooltipMessage("Что-то пошло не так! Попробуйте ещё раз.");
+        console.log(err);
+      });
+  }
+
+  function onRegister(data) {
+    return mainApi
+      .createUser(data)
+      .then(() => {
+        history.push("/signin");
+        setStatusMessage(true);
+        setTooltipMessage("Вы успешно зарегистрировались!");
+      })
+      .catch((err) => {
+        setStatusMessage(false);
+        setTooltipMessage("Что-то пошло не так! Попробуйте ещё раз.");
+        console.log(err);
+      })
+      .finally(() => {
+        handleInfoTooltipClick();
+      });
+  }
+
+  function onSignout() {
+    mainApi
+   .logout()
+   .then(()=> {
+     setIsLoggedIn(false);
+     history.push("/signin");
+   });
+ }
 
   return (
-    <section className="app">
-      <Switch>
-        <Route exact path="/">
-        <Header />
-        <Main />
-        <Footer />
-        </Route>
-        <Route path="/movies">
-        <Header 
-        isLoggedIn={isLoggedIn}/>
-          <Movies />
-          <Footer />
-        </Route>
-        <Route path="/saved-movies">
-        <Header 
-        isLoggedIn={isLoggedIn}/>
-          <SavedMovies />
-          <Footer />
-        </Route>
-        <Route path="/profile">
-          <Header />
-          <Profile />
-        </Route>
-        <Route path="/signup">
-          <Register/>
-        </Route>
-        <Route path="/signin">
-          <Login/>
-        </Route>
-        <Route path="*">
-          <NotFound />
-        </Route>
-      </Switch>
-      
-      <SidebarMenu></SidebarMenu>
-    </section>
+    <CurrentUserContext.Provider value={currentUser}>
+      <section className="app">
+        <Switch>
+          <Route exact path="/">
+            <Header />
+            <Main />
+            <Footer />
+          </Route>
+          <Route path="/movies">
+            <Header isLoggedIn={isLoggedIn} />
+            <Movies />
+            <Footer />
+          </Route>
+          <Route path="/saved-movies">
+            <Header isLoggedIn={isLoggedIn} />
+            <SavedMovies />
+            <Footer />
+          </Route>
+          <Route path="/profile">
+            <Header />
+            <Profile />
+          </Route>
+          <Route path="/signup">
+            <Register />
+          </Route>
+          <Route path="/signin">
+            <Login />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+
+        <SidebarMenu></SidebarMenu>
+      </section>
+    </CurrentUserContext.Provider>
   );
 }
 
---
+++ src/utils/MainApi.js
@@ -0,0 +1,111 @@
+export default class MainApi {
+  constructor(url, token) {
+    this._url = url;
+    this._token = token;
+    this._headers = {
+      "Content-type": "application/json",
+    };
+  }
+
+  async _checkResult(response) {
+    if (res.ok) {
+      const resData = await res.json();
+      return resData.data;
+    }
+    return Promise.reject(new Error(`Ошибка ${response.status}: ${response.statusText}`)
+    );
+  }
+
+  createUser(name, email, password) {
+    return fetch(`${this._url}/signup`, {
+      method: "POST",
+      headers: this._headers,
+      credentials: "include",
+      body: JSON.stringify({
+        name,
+        email,
+        password,
+      }),
+    }).then(this._checkResult);
+  }
+
+  login(email, password) {
+    return fetch(`${this._url}/signin`, {
+      method: "POST",
+      headers: this._headers,
+      credentials: "include",
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    }).then(this._checkResult);
+  }
+
+  logout = () => {
+    return fetch(`${this._url}/signout`, {
+      method: "POST",
+      headers: this._headers,
+      credentials: 'include',
+    }).then(this._checkResult);
+  }
+
+  getUserInfo() {
+    return fetch(`${this._url}/users/me`, {
+      method: "GET",
+      headers: this._headers,
+      credentials: "include",
+    }).then(this._checkResult);
+  }
+
+  updateUser(userData) {
+    return fetch(`${this._url}/users/me/`, {
+      method: "PATCH",
+      headers: this._headers,
+      credentials: "include",
+      body: JSON.stringify({
+        name: userData.name,
+        email: userData.email,
+      }),
+    }).then(this._checkResult);
+  }
+
+  //сохраненные фильмы 
+  getMovies() {
+    return fetch(`${this._url}/movies`, {
+      method: "GET",
+      headers: this._headers,
+      credentials: "include",
+    }).then(this._checkResult);
+  }
+
+  createMovie(data) {
+    return fetch(`${this._url}/movies`, {
+      method: "POST",
+      headers: this._headers,
+      credentials: "include",
+      body: JSON.stringify({
+        country: data.country,
+        director: data.director,
+        duration: data.duration,
+        year: data.year,
+        description: data.description,
+        image: data.image,
+        trailerLink: data.trailerLink,
+        thumbnail: data.thumbnail,
+        movieId: data.id,
+        nameRU: data.nameRU,
+        nameEN: data.nameEN,
+      }),
+    }).then(this._checkResult);
+  }
+
+  deleteMovie(movieId) {
+    return fetch(`${this._url}/movies/${movieId}`, {
+      method: "DELETE",
+      headers: this._headers,
+      credentials: "include",
+    }).then(this._checkResult);
+  }
+}
+
+const mainApi = new MainApi("https://back.movie-explorer.nomoredomains.icu");
